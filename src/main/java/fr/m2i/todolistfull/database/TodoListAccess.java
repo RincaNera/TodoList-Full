db7800aa96c6098ba734715bf006ea0b16dfa31a
LE GLOUX Thomas <legloux.thomas@gmail.com>
2023-03-03 17:14:43 +0100

    mysql code todolist access added

--- src/main/java/fr/m2i/todolistfull/database/TodoListAccess.java
+++ src/main/java/fr/m2i/todolistfull/database/TodoListAccess.java
@@ -3,6 +3,17 @@ package fr.m2i.todolistfull.database;
 public class TodoListAccess {
     private final DatabaseAccess db;
 
+
+    private final String GETBYUSER = "SELECT idTodo, titleTodo, descriptionTodo, dateTodo, idUrgency, idUser, nameUrgency, lastNameUser, firstNameUser " +
+            "FROM Todo WHERE idUser = ? INNER JOIN Urgency " +
+            "ON Urgency.idUrgency = Todo.idUrgency INNER JOIN User ON User.idUser= Todo.idUser";
+    private final String GETBYURGENCY = "SELECT idTodo, titleTodo, descriptionTodo, dateTodo, idUrgency, idUser, nameUrgency, lastNameUser, firstNameUser " +
+            "FROM Todo WHERE idUrgency = ? INNER JOIN Urgency " +
+            "ON Urgency.idUrgency = Todo.idUrgency INNER JOIN User ON User.idUser= Todo.idUser";
+    private final String GETBYURGENCYANDUSER = "SELECT idTodo, titleTodo, descriptionTodo, dateTodo, idUrgency, idUser, nameUrgency, lastNameUser, firstNameUser " +
+            "FROM Todo WHERE idUrgency = ? , idUser = ? INNER JOIN Urgency" +
+            "ON Urgency.idUrgency = Todo.idUrgency INNER JOIN User ON User.idUser= Todo.idUser";
+
     public TodoListAccess(DatabaseAccess db) {
         this.db = db;
     }
